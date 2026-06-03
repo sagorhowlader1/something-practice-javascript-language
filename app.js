0ce9547028de70d3e0bbb93a9cd34373867b25c7
sagorhowlader1 <sagorhowlader.edu@gmail.com>
2026-06-03 15:13:10 +0600

    added an something practice javascript  code

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log("Sagor Howlader");
+
+console.log(37*1998);
+
+
+let myName = "Sagor";
+myName = "Prime Misiter"
+console.log(myName);
+
+let myAge = 25;
+console.log(myAge);
+
+// String, Number/Integer, Boolean
+
+// String
+let name = "Sagor Howlader"
+let age = 25;
+let myHappy = true;
+// console.log(name);
+// console.log(age);
+console.log(String(name) + age + Boolean(myHappy));
+
+// Number/Interger
+let score = 200;
+let scores = 400;
+console.log(score + scores);
+
+// Boolean
+let  areYousick = false;
+let areYouHappy = true;
+console.log(areYouHappy);
