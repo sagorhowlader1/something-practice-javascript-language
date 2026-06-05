00816037d99a715f98e8ac07369c877dbdc90dbd
sagorhowlader1 <sagorhowlader.edu@gmail.com>
2026-06-05 19:55:47 +0600

    added an arithmetic operater add

---
+++ arithmetic.js
@@ -0,0 +1,9 @@
+let cow = 5;
+let price = 50000;
+let people = 4;
+
+// const totalPrice = price * cow;
+// console.log(totalPrice);
+
+const remainder = cow % people;
+console.log(remainder);
---
+++ function.js
@@ -0,0 +1,47 @@
+// function calculateArea(number){
+//     let area = number * number;
+//     console.log(area)
+// }
+
+// calculateArea(2)
+
+// function calculateArea (number){
+//     let area = number * number;
+//     return area;
+//     // console.log(area)
+// }
+
+// console.log(calculateArea (4))
+
+
+function calculateArea (number){
+    let area = number * number;
+    return area;
+    // console.log(area)
+}
+
+const result = calculateArea (4)
+
+function calculateMoney(result){
+    const totalMoney = result * 20;
+    return totalMoney;
+}
+
+const moneyResult = calculateMoney(result)
+console.log(moneyResult)
+
+
+// function latterName (number){
+//     let price = 5 + 2;
+//     console.log(price);
+// }
+
+// latterName(2)
+
+function latterName (number){
+    let price = 5 + 2;
+    return price;
+    // console.log(price);
+}
+
+console.log(latterName(2)) 
