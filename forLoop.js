cb33cda1f3a2e62e83ddf70724a61255f50c35e5
sagorhowlader1 <sagorhowlader.edu@gmail.com>
2026-06-08 00:01:08 +0600

    added an breakandcontinue.js file add now

---
+++ forLoop.js
@@ -0,0 +1,29 @@
+// for(let i = 0; i < 100; i++){
+//     if(i % 2 == 0){
+//         console.log("Even Number - ", i);
+//     }else{
+//         console.log("Odd Number - ", i);
+//     }
+// }
+
+// for(let i = 0; i < 100; i++){
+//     if(i % 2 == 0){
+//         console.log("Even");
+//     }else{
+//         console.log("Odd");
+//     }
+// }
+
+// let x = 0;
+// while(x < 100){
+//     if(x % 2 == 0){
+//         console.log("Even number - ", x);
+//     }else{
+//         console.log("Odd number -", x);
+//     }
+//     x++
+// }
+
+for(let i = 100; i >=1; i--){
+    console.log(i);
+}
